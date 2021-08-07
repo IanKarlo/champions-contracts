ee1006f35e22ab20d5633b5ae51937e271c74b81
Samuel Simões <samuelsimoes31@gmail.com>
2021-08-07 15:54:12 -0300

    Inserindo personagens e items

---
+++ Projeto Físico/itens.sql
@@ -0,0 +1,95 @@
+INSERT INTO ITEM(NOME, ATR_ATAQUE, ATR_VELOCIDADE, ATR_ARMADURA, ATR_PODER)
+VALUES ('Leque de Piparo',27,10,0,0);
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Leque de Piparo', 'Aumenta o dano de ataques de vento');
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Leque de Piparo', 'Seus ataques bloqueiam e refletem projetos');
+
+INSERT INTO ITEM(NOME, ATR_ATAQUE, ATR_VELOCIDADE, ATR_ARMADURA, ATR_PODER)
+VALUES ('Armadura Pangolim',0,0,36,0);
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Armadura Pangolim', 'Receba menos danos de todos os ataques, caso você não esteja em crowd control.');
+
+INSERT INTO ITEM(NOME, ATR_ATAQUE, ATR_VELOCIDADE, ATR_ARMADURA, ATR_PODER)
+VALUES ('Cetro das Fadas',30,10,0,0);
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Cetro das Fadas', 'Aumenta o danos de ataques de água.');
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Cetro das Fadas', 'Fique mais rápido quando na água.');
+
+INSERT INTO ITEM(NOME, ATR_ATAQUE, ATR_VELOCIDADE, ATR_ARMADURA, ATR_PODER)
+VALUES ('Algas Lunares',20,0,0,0);
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Algas Lunares', 'Todos os seus ataques de água agora aplicam slow.');
+
+INSERT INTO ITEM(NOME, ATR_ATAQUE, ATR_VELOCIDADE, ATR_ARMADURA, ATR_PODER)
+VALUES ('Capa de Fogo',0,0,37,20);
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Capa de Fogo', 'Resista todos os ataques de fogo.');
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Capa de Fogo', 'Armazene mais energia térmica.');
+
+INSERT INTO ITEM(NOME, ATR_ATAQUE, ATR_VELOCIDADE, ATR_ARMADURA, ATR_PODER)
+VALUES ('Cajado de Tesla',0,22,0,47);
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Cajado de Tesla', 'Todas as suas habilidades elétricas dão mais dano');
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Cajado de Tesla', 'Todas as suas habilidades elétricas possuem mais range');
+
+INSERT INTO ITEM(NOME, ATR_ATAQUE, ATR_VELOCIDADE, ATR_ARMADURA, ATR_PODER)
+VALUES ('Railgun',0,19,0,15);
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Railgun', 'Ganhe knockback em todos seus ataques');
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Railgun', 'Ganhe bônus elétrico em todos seus ataques');
+
+INSERT INTO ITEM(NOME, ATR_ATAQUE, ATR_VELOCIDADE, ATR_ARMADURA, ATR_PODER)
+VALUES ('Armadura Magnética',0,0,38,0);
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Armadura Magnética', 'Ataques em projéteis serão desviados em sua direção');
+
+INSERT INTO ITEM(NOME, ATR_ATAQUE, ATR_VELOCIDADE, ATR_ARMADURA, ATR_PODER)
+VALUES ('Tridente de Gelo',0,0,0,32);
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Tridente de Gelo', 'Todos os seus ataques podem fazer o personagem congelado');
+
+INSERT INTO ITEM(NOME, ATR_ATAQUE, ATR_VELOCIDADE, ATR_ARMADURA, ATR_PODER)
+VALUES ('Balaços Dançantes',0,21,0,0);
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Balaços Dançantes', 'Quanto mais você ataca, mais velocidade de ataque você tem');
+
+INSERT INTO ITEM(NOME, ATR_ATAQUE, ATR_VELOCIDADE, ATR_ARMADURA, ATR_PODER)
+VALUES ('Veneno de Froigon',23,0,0,30);
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Veneno de Froigon', 'Todos os seus ataques diminuem armadura');
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Veneno de Froigon', 'Seus ataques causam dano de veneno');
+
+INSERT INTO ITEM(NOME, ATR_ATAQUE, ATR_VELOCIDADE, ATR_ARMADURA, ATR_PODER)
+VALUES ('Pó de Papipolem',0,0,20,20);
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Pó de Papipolem', 'Efeitos de cura em você são aumentados');
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Pó de Papipolem', 'Efeitos de cura que você causa são aumentados');
+
+INSERT INTO ITEM(NOME, ATR_ATAQUE, ATR_VELOCIDADE, ATR_ARMADURA, ATR_PODER)
+VALUES ('Capa de Refração',0,0,0,50);
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Capa de Refração', 'Ao ser ativada, equanto você ficar imóvel fique invisível');
+
+INSERT INTO ITEM(NOME, ATR_ATAQUE, ATR_VELOCIDADE, ATR_ARMADURA, ATR_PODER)
+VALUES ('Arco Guiado',0,15,0,30);
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Arco Guiado', 'Todos os seus ataque a longa distância causam mais dano');
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Arco Guiado', 'Todos os seus ataque a longa distância não podem ser parados');
+
+INSERT INTO ITEM(NOME, ATR_ATAQUE, ATR_VELOCIDADE, ATR_ARMADURA, ATR_PODER)
+VALUES ('Blimps Flutuantes',0,37,0,0);
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Blimps Flutuantes', 'Enquanto você tiver energia, fique mais rápido');
+
+INSERT INTO ITEM(NOME, ATR_ATAQUE, ATR_VELOCIDADE, ATR_ARMADURA, ATR_PODER)
+VALUES ('Lança Giratória',50,0,0,0);
+INSERT INTO BUFFS(NOME, DESCRICAO)
+VALUES ('Lança Giratória', 'Ataque contínuos causam mais dano ao longo do tempo');
