a5d306fdd63352c492139aeda43965d26216655d
ThallysRocha <51234771+ThallysRocha@users.noreply.github.com>
2021-08-24 21:24:25 -0300

    Populando a tabela Obteve

---
+++ Modelo Físico/Obteve.sql
@@ -0,0 +1,189 @@
+INSERT INTO OBTEVE VALUES(1,'Ferkun','Tridente de Gelo');
+INSERT INTO OBTEVE VALUES(1,'Ferkun','Veneno de Froigon');
+INSERT INTO OBTEVE VALUES(1,'Ferkun','Cajado de Tesla');
+
+INSERT INTO OBTEVE VALUES(2,'Kneallemont','Leque de Piparo');
+INSERT INTO OBTEVE VALUES(2,'Kneallemont','Capa de Fogo');
+INSERT INTO OBTEVE VALUES(2,'Kneallemont','Algas Lunares');
+INSERT INTO OBTEVE VALUES(2,'Kneallemont','Veneno de Froigon');
+INSERT INTO OBTEVE VALUES(2,'Kneallemont','Cajado de Tesla');
+INSERT INTO OBTEVE VALUES(2,'Kneallemont','Armadura Pangolim');
+INSERT INTO OBTEVE VALUES(2,'Kneallemont','Pó de Papipolem');
+INSERT INTO OBTEVE VALUES(2,'Kneallemont','Railgun');
+
+INSERT INTO OBTEVE VALUES(3,'NIcolashOfSpark','Capa de Refração');
+INSERT INTO OBTEVE VALUES(3,'NIcolashOfSpark','Algas Lunares');
+
+INSERT INTO OBTEVE VALUES(4,'Thedgme','Armadura Pangolim');
+INSERT INTO OBTEVE VALUES(4,'Thedgme','Leque de Piparo');
+
+INSERT INTO OBTEVE VALUES(5,'Deplivars','Capa de Fogo');
+INSERT INTO OBTEVE VALUES(5,'Deplivars','Cetro das Fadas');
+INSERT INTO OBTEVE VALUES(5,'Deplivars','Railgun');
+
+INSERT INTO OBTEVE VALUES(6,'Overtutremew','Capa de Refração');
+
+INSERT INTO OBTEVE VALUES(7,'Deasind1987','Armadura Magnética');
+INSERT INTO OBTEVE VALUES(7,'Deasind1987','Blimps Flutuantes');
+INSERT INTO OBTEVE VALUES(7,'Deasind1987','Cetro das Fadas');
+
+INSERT INTO OBTEVE VALUES(8,'Allontention','Algas Lunares');
+
+INSERT INTO OBTEVE VALUES(9,'Difter','Cajado de Tesla');
+INSERT INTO OBTEVE VALUES(9,'Difter','Armadura Pangolim');
+INSERT INTO OBTEVE VALUES(9,'Difter','Veneno de Froigon');
+
+INSERT INTO OBTEVE VALUES(10,'Yemand','Leque de Piparo');
+INSERT INTO OBTEVE VALUES(10,'Yemand','Balaços Dançantes');
+INSERT INTO OBTEVE VALUES(10,'Yemand','Armadura Magnética');
+INSERT INTO OBTEVE VALUES(10,'Yemand','Cajado de Tesla');
+
+INSERT INTO OBTEVE VALUES(11,'Whavence','Tridente de Gelo');
+
+INSERT INTO OBTEVE VALUES(12,'Glan2006','Cajado de Tesla');
+INSERT INTO OBTEVE VALUES(12,'Glan2006','Blimps Flutuantes');
+INSERT INTO OBTEVE VALUES(12,'Glan2006','Capa de Refração');
+INSERT INTO OBTEVE VALUES(12,'Glan2006','Veneno de Froigon');
+INSERT INTO OBTEVE VALUES(12,'Glan2006','Armadura Magnética');
+INSERT INTO OBTEVE VALUES(12,'Glan2006','Leque de Piparo');
+INSERT INTO OBTEVE VALUES(12,'Glan2006','Pó de Papipolem');
+INSERT INTO OBTEVE VALUES(12,'Glan2006','Railgun');
+
+INSERT INTO OBTEVE VALUES(13,'Coleaked','Tridente de Gelo');
+INSERT INTO OBTEVE VALUES(13,'Coleaked','Cetro das Fadas');
+INSERT INTO OBTEVE VALUES(13,'Coleaked','Blimps Flutuantes');
+INSERT INTO OBTEVE VALUES(13,'Coleaked','Lança Giratória');
+INSERT INTO OBTEVE VALUES(13,'Coleaked','Veneno de Froigon');
+
+INSERT INTO OBTEVE VALUES(14,'Alled2000','Armadura Pangolim');
+INSERT INTO OBTEVE VALUES(14,'Alled2000','Lança Giratória');
+INSERT INTO OBTEVE VALUES(14,'Alled2000','Veneno de Froigon');
+
+INSERT INTO OBTEVE VALUES(15,'Eventer','Pó de Papipolem');
+INSERT INTO OBTEVE VALUES(15,'Eventer','Blimps Flutuantes');
+INSERT INTO OBTEVE VALUES(15,'Eventer','Algas Lunares');
+INSERT INTO OBTEVE VALUES(15,'Eventer','Leque de Piparo');
+INSERT INTO OBTEVE VALUES(15,'Eventer','Tridente de Gelo');
+
+INSERT INTO OBTEVE VALUES(16,'Excul2002','Cetro das Fadas');
+INSERT INTO OBTEVE VALUES(16,'Excul2002','Capa de Fogo');
+INSERT INTO OBTEVE VALUES(16,'Excul2002','Leque de Piparo');
+
+INSERT INTO OBTEVE VALUES(17,'Exach1985','Balaços Dançantes');
+INSERT INTO OBTEVE VALUES(17,'Exach1985','Cajado de Tesla');
+INSERT INTO OBTEVE VALUES(17,'Exach1985','Armadura Pangolim');
+
+INSERT INTO OBTEVE VALUES(19,'Dithey','Algas Lunares');
+INSERT INTO OBTEVE VALUES(19,'Dithey','Railgun');
+INSERT INTO OBTEVE VALUES(19,'Dithey','Cajado de Tesla');
+INSERT INTO OBTEVE VALUES(19,'Dithey','Armadura Magnética');
+INSERT INTO OBTEVE VALUES(19,'Dithey','Armadura Pangolim');
+INSERT INTO OBTEVE VALUES(19,'Dithey','Veneno de Froigon');
+INSERT INTO OBTEVE VALUES(19,'Dithey','Arco Guiado');
+INSERT INTO OBTEVE VALUES(19,'Dithey','Capa de Fogo');
+
+INSERT INTO OBTEVE VALUES(20,'Upostionots','Armadura Pangolim');
+INSERT INTO OBTEVE VALUES(20,'Upostionots','Lança Giratória');
+
+INSERT INTO OBTEVE VALUES(21,'Theighavell08','Algas Lunares');
+INSERT INTO OBTEVE VALUES(21,'Theighavell08','Arco Guiado');
+INSERT INTO OBTEVE VALUES(21,'Theighavell08','Cajado de Tesla');
+INSERT INTO OBTEVE VALUES(21,'Theighavell08','Pó de Papipolem');
+INSERT INTO OBTEVE VALUES(21,'Theighavell08','Balaços Dançantes');
+INSERT INTO OBTEVE VALUES(21,'Theighavell08','Cetro das Fadas');
+INSERT INTO OBTEVE VALUES(21,'Theighavell08','Capa de Refração');
+
+INSERT INTO OBTEVE VALUES(22,'Sithen','Armadura Magnética');
+INSERT INTO OBTEVE VALUES(22,'Sithen','Veneno de Froigon');
+
+INSERT INTO OBTEVE VALUES(23,'Gribladeen1992','Armadura Magnética');
+INSERT INTO OBTEVE VALUES(23,'Gribladeen1992','Balaços Dançantes');
+INSERT INTO OBTEVE VALUES(23,'Gribladeen1992','Tridente de Gelo');
+INSERT INTO OBTEVE VALUES(23,'Gribladeen1992','Arco Guiado');
+INSERT INTO OBTEVE VALUES(23,'Gribladeen1992','Pó de Papipolem');
+INSERT INTO OBTEVE VALUES(23,'Gribladeen1992','Lança Giratória');
+INSERT INTO OBTEVE VALUES(23,'Gribladeen1992','Algas Lunares');
+INSERT INTO OBTEVE VALUES(23,'Gribladeen1992','Cajado de Tesla');
+
+INSERT INTO OBTEVE VALUES(24,'Ableas','Cetro das Fadas');
+INSERT INTO OBTEVE VALUES(24,'Ableas','Balaços Dançantes');
+INSERT INTO OBTEVE VALUES(24,'Ableas','Tridente de Gelo');
+INSERT INTO OBTEVE VALUES(24,'Ableas','Cajado de Tesla');
+INSERT INTO OBTEVE VALUES(24,'Ableas','Arco Guiado');
+
+INSERT INTO OBTEVE VALUES(25,'Brainitterem','Armadura Magnética');
+INSERT INTO OBTEVE VALUES(25,'Brainitterem','Blimps Flutuantes');
+INSERT INTO OBTEVE VALUES(25,'Brainitterem','Capa de Fogo');
+INSERT INTO OBTEVE VALUES(25,'Brainitterem','Railgun');
+INSERT INTO OBTEVE VALUES(25,'Brainitterem','Veneno de Froigon');
+
+INSERT INTO OBTEVE VALUES(26,'Sithereony','Cajado de Tesla');
+INSERT INTO OBTEVE VALUES(26,'Sithereony','Lança Giratória');
+INSERT INTO OBTEVE VALUES(26,'Sithereony','Leque de Piparo');
+
+INSERT INTO OBTEVE VALUES(27,'Inihis','Algas Lunares');
+
+INSERT INTO OBTEVE VALUES(29,'Fating','Pó de Papipolem');
+INSERT INTO OBTEVE VALUES(29,'Fating','Tridente de Gelo');
+INSERT INTO OBTEVE VALUES(29,'Fating','Veneno de Froigon');
+INSERT INTO OBTEVE VALUES(29,'Fating','Cajado de Tesla');
+INSERT INTO OBTEVE VALUES(29,'Fating','Armadura Pangolim');
+INSERT INTO OBTEVE VALUES(29,'Fating','Capa de Fogo');
+INSERT INTO OBTEVE VALUES(29,'Fating','Capa de Refração');
+INSERT INTO OBTEVE VALUES(29,'Fating','Railgun');
+
+INSERT INTO OBTEVE VALUES(30,'Peopeor','Arco Guiado');
+INSERT INTO OBTEVE VALUES(30,'Peopeor','Blimps Flutuantes');
+INSERT INTO OBTEVE VALUES(30,'Peopeor','Leque de Piparo');
+
+INSERT INTO OBTEVE VALUES(31,'Squing','Leque de Piparo');
+INSERT INTO OBTEVE VALUES(31,'Squing','Capa de Fogo');
+INSERT INTO OBTEVE VALUES(31,'Squing','Arco Guiado');
+INSERT INTO OBTEVE VALUES(31,'Squing','Algas Lunares');
+
+INSERT INTO OBTEVE VALUES(32,'Tatromer','Tridente de Gelo');
+
+INSERT INTO OBTEVE VALUES(33,'Eyettaking','Lança Giratória');
+INSERT INTO OBTEVE VALUES(33,'Eyettaking','Pó de Papipolem');
+INSERT INTO OBTEVE VALUES(33,'Eyettaking','Blimps Flutuantes');
+INSERT INTO OBTEVE VALUES(33,'Eyettaking','Cetro das Fadas');
+
+INSERT INTO OBTEVE VALUES(34,'Eyettaking','Railgun');
+INSERT INTO OBTEVE VALUES(34,'Eyettaking','Capa de Refração');
+INSERT INTO OBTEVE VALUES(34,'Eyettaking','Cetro das Fadas');
+INSERT INTO OBTEVE VALUES(34,'Eyettaking','Blimps Flutuantes');
+
+INSERT INTO OBTEVE VALUES(35,'Harriew','Capa de Refração');
+INSERT INTO OBTEVE VALUES(35,'Harriew','Railgun');
+INSERT INTO OBTEVE VALUES(35,'Harriew','Lança Giratória');
+INSERT INTO OBTEVE VALUES(35,'Harriew','Blimps Flutuantes');
+INSERT INTO OBTEVE VALUES(35,'Harriew','Veneno de Froigon');
+INSERT INTO OBTEVE VALUES(35,'Harriew','Pó de Papipolem');
+
+INSERT INTO OBTEVE VALUES(36,'MeloPinto','Capa de Refração');
+INSERT INTO OBTEVE VALUES(36,'MeloPinto','Arco Guiado');
+
+INSERT INTO OBTEVE VALUES(37,'Forded','Capa de Refração');
+INSERT INTO OBTEVE VALUES(37,'Forded','Tridente de Gelo');
+INSERT INTO OBTEVE VALUES(37,'Forded','Leque de Piparo');
+INSERT INTO OBTEVE VALUES(37,'Forded','Armadura Magnética');
+INSERT INTO OBTEVE VALUES(37,'Forded','Armadura Pangolim');
+INSERT INTO OBTEVE VALUES(37,'Forded','Blimps Flutuantes');
+
+INSERT INTO OBTEVE VALUES(38,'Brind1989','Arco Guiado');
+INSERT INTO OBTEVE VALUES(38,'Brind1989','Capa de Refração');
+INSERT INTO OBTEVE VALUES(38,'Brind1989','Cetro das Fadas');
+INSERT INTO OBTEVE VALUES(38,'Brind1989','Algas Lunares');
+INSERT INTO OBTEVE VALUES(38,'Brind1989','Veneno de Froigon');
+INSERT INTO OBTEVE VALUES(38,'Brind1989','Cajado de Tesla');
+
+INSERT INTO OBTEVE VALUES(39,'Anyinct','Blimps Flutuantes');
+INSERT INTO OBTEVE VALUES(39,'Anyinct','Capa de Refração');
+INSERT INTO OBTEVE VALUES(39,'Anyinct','Leque de Piparo');
+INSERT INTO OBTEVE VALUES(39,'Anyinct','Pó de Papipolem');
+INSERT INTO OBTEVE VALUES(39,'Anyinct','Tridente de Gelo');
+INSERT INTO OBTEVE VALUES(39,'Anyinct','Veneno de Froigon');
+INSERT INTO OBTEVE VALUES(39,'Anyinct','Armadura Magnética');
+INSERT INTO OBTEVE VALUES(39,'Anyinct','Balaços Dançantes');
+
+INSERT INTO OBTEVE VALUES(40,'Paits','Lança Giratória');
