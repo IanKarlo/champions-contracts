1a83be7be7a4744d6cc0dc5fada3cee8e61fe158
Alef-0 <AlefGabryel@protonmail.com>
2021-08-26 08:26:16 -0300

    Edits para rodar no sql

--- Modelo Físico/funcoesProcedures.sql
+++ Modelo Físico/funcoesProcedures.sql
@@ -1,24 +1,23 @@
 ---------- firstIsFriendSecond-------------
-
-create or replace NONEDITIONABLE PROCEDURE firstIsFriendSecond(id1 IN jogador.id%TYPE, nick1 IN jogador.nickname%TYPE, id2 IN jogador.id%TYPE, nick2 IN jogador.nickname%TYPE)
-    IS
-    qtd NUMBER;
-
-    BEGIN
-        SELECT COUNT(*) INTO qtd FROM AMIZADE A WHERE (A.lista_nick = nick1 AND a.lista_id = id1) AND (A.amigo_nick = nick2 AND a.amigo_id = id2);
-        IF qtd > 0 THEN
-          dbms_output.put_line(nick1||' é amigo de '||nick2);
-        ELSE
-            dbms_output.put_line(nick1||' não é amigo de '||nick2);
-        END IF;
-    END;
- --------------------------FUNÇÃO KDA--------------------------
- create or replace NONEDITIONABLE FUNCTION KDA (abates NUMBER,assistencias NUMBER, mortes NUMBER) RETURN
-NUMBER IS
-    kda NUMBER;
+create or replace PROCEDURE firstIsFriendSecond(id1 IN jogador.id%TYPE, nick1 IN jogador.nickname%TYPE, id2 IN jogador.id%TYPE, nick2 IN jogador.nickname%TYPE)
+IS
+qtd NUMBER;
 BEGIN
+    SELECT COUNT(*) INTO qtd FROM AMIZADE A WHERE (A.lista_nick = nick1 AND a.lista_id = id1) AND (A.amigo_nick = nick2 AND a.amigo_id = id2);
+    IF qtd > 0 THEN
+        dbms_output.put_line(nick1||' é amigo de '||nick2);
+    ELSE
+    dbms_output.put_line(nick1||' não é amigo de '||nick2);
+    END IF;
+END;
+---------------
 
-    BEGIN           
+ --------------------------FUNÇÃO KDA--------------------------
+create or replace FUNCTION KDA (abates NUMBER,assistencias NUMBER, mortes NUMBER) RETURN
+NUMBER IS
+kda NUMBER;
+BEGIN
+    BEGIN
         IF (mortes<=0) THEN
             kda := (abates+(assistencias/2));
         ELSE
@@ -27,39 +26,38 @@ BEGIN
     END;
     RETURN kda;
 END;
------------------------------FUNÇÃO KDA POR PARTIDA DE UM JOGADOR-------------------------------------------
+---------------------------------------------------------
 
-create or replace NONEDITIONABLE FUNCTION KDAP(ID participa.id%TYPE,NICK participa.nickname%TYPE,COD partida.codigo%TYPE ) RETURN
-NUMBER IS 
+-----------------------------FUNÇÃO KDA POR PARTIDA DE UM JOGADOR-------------------------------------------
+create or replace FUNCTION KDAP(ID participa.id%TYPE,NICK participa.nickname%TYPE,COD partida.codigo%TYPE ) 
+RETURN NUMBER IS
     kdaP NUMBER;
     BEGIN
-        SELECT KDA(P.KILL,P.ASSIST,P.DEATH) INTO kdap 
+        SELECT KDA(P.KILL,P.ASSIST,P.DEATH) INTO kdap
         FROM participa P
         WHERE p.codigo = COD AND P.ID = ID AND P.NICKNAME = NICK;
         RETURN kdap;
     END;
-    --------------------------------DICIONÁRIO-----------------------------------------------------------
-create or replace NONEDITIONABLE PROCEDURE DICIONARIO(STR VARCHAR2) 
-IS
-BEGIN
-    FOR LINHA IN (SELECT * FROM (SELECT P.NOME 
-                             FROM PERSONAGEM P
-                             UNION
-                             SELECT I.NOME
-                             FROM ITEM I) UN WHERE UPPER(UN.NOME) LIKE '%'||UPPER(STR)||'%')
+-----------------------------
+
+--------------------------------DICIONÁRIO-----------------------------------------------------------
+create or replace PROCEDURE DICIONARIO(STR VARCHAR2) IS BEGIN
+    FOR LINHA IN (SELECT * FROM
+        (
+        SELECT P.NOME FROM PERSONAGEM P
+        UNION
+        SELECT I.NOME FROM ITEM I
+        )
+        UN WHERE UPPER(UN.NOME) LIKE '%'||UPPER(STR)||'%')
     LOOP
-
-        dbms_output.put_line(LINHA.NOME);
-
-    END LOOP;                   
-
+    dbms_output.put_line(LINHA.NOME);
+    END LOOP;
 END;
+------------------------------------
 
 -------------------------------------TOTAL DE VITÓRIAS DE UM JOGADOR---------------------------------------
-CREATE OR REPLACE FUNCTION VITORIAS_JOGADOR(in_id JOGADOR.ID%TYPE, in_nick JOGADOR.NICKNAME%TYPE) 
-RETURN NUMBER
-IS
-ret NUMBER(8);
+CREATE OR REPLACE FUNCTION VITORIAS_JOGADOR(in_id JOGADOR.ID%TYPE, in_nick JOGADOR.NICKNAME%TYPE)
+RETURN NUMBER IS ret NUMBER(8);
 BEGIN
     FOR LINHA IN (  SELECT J.ID, J.NICKNAME, COUNT(P.CODIGO) as QTD
                     FROM JOGADOR J LEFT OUTER JOIN PARTICIPA P ON P.NICKNAME = J.NICKNAME AND J.ID = P.ID
@@ -67,9 +65,8 @@ BEGIN
                     GROUP BY (J.ID, J.NICKNAME)
                     ORDER BY J.ID, J.NICKNAME)
     LOOP
-        ret := LINHA.QTD;
+    ret := LINHA.QTD;
     END LOOP;
-    
     return ret;
 END;
 -----------------------------------------------------------------
--- Modelo Físico/triggers.sql
+++ Modelo Físico/triggers.sql
@@ -1,30 +1,27 @@
 -- Trigger Inseção em Participa
-CREATE OR REPLACE TRIGGER INSERCAO_PARTICIPA
-BEFORE INSERT ON PARTICIPA
-FOR EACH ROW
-DECLARE
+CREATE OR REPLACE TRIGGER INSERCAO_PARTICIPA BEFORE INSERT ON PARTICIPA FOR EACH ROW DECLARE
     cntr_total NUMBER(1) := 0;
     cntr_time NUMBER(1) := 0;
-    
     is_in NUMBER(1) := 0;
 BEGIN
-    
-    FOR reg_pXp IN (SELECT *
-                    FROM PARTIDA P1 INNER JOIN PARTICIPA P2 ON P1.CODIGO = P2.CODIGO
-                    WHERE P1.CODIGO = :NEW.CODIGO)
+    FOR reg_pXp IN
+        (SELECT *
+        FROM PARTIDA P1 INNER JOIN PARTICIPA P2 ON P1.CODIGO = P2.CODIGO
+        WHERE P1.CODIGO = :NEW.CODIGO)
     LOOP
-        cntr_total := cntr_total + 1;
-        IF reg_pXp.TIME = :NEW.TIME THEN
-            cntr_time := cntr_time + 1;
-        END IF;
-        
-        IF cntr_time >= 3 THEN
-            RAISE_APPLICATION_ERROR(-20205, 'Time já está cheio\n');
-        ELSIF cntr_total>=6 THEN
-            RAISE_APPLICATION_ERROR(-20205, 'Partida já está lotada\n');
-        END IF;
+    cntr_total := cntr_total + 1;
+
+    IF reg_pXp.TIME = :NEW.TIME
+    THEN cntr_time := cntr_time + 1;
+    END IF;
+
+    IF cntr_time >= 3
+    THEN RAISE_APPLICATION_ERROR(-20205, 'Time já está cheio\n');
+    ELSIF cntr_total >= 6
+    THEN RAISE_APPLICATION_ERROR(-20205, 'Partida já está lotada\n');
+    END IF;
+
     END LOOP;
-    
     FOR reg_p IN (SELECT * FROM POSSUI WHERE :NEW.ID = POSSUI.ID AND :NEW.NICKNAME = POSSUI.NICK)
     LOOP
         IF(reg_p.NOME = :NEW.CAMPEAO) THEN
@@ -32,8 +29,8 @@ BEGIN
         END IF;
     EXIT WHEN is_in = 1;
     END LOOP;
-    
-    IF is_in = 0 THEN
-        RAISE_APPLICATION_ERROR(-20205, 'Jogador não possui esse personagem\n');
+
+    IF is_in = 0
+    THEN RAISE_APPLICATION_ERROR(-20205, 'Jogador não possui esse personagem\n');
     END IF;
 END;
